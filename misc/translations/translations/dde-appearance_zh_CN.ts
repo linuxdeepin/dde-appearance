7c15ab31a60510e41bfd18f2a7489fe3a9698576
caixiangrong <caixiangrong@uniontech.com>
2022-11-21 17:56:13 +0800

    fix: 更新翻译文件
    更新个性化通知的翻译文件
    
    Log: 更新翻译
    Task: https://pms.uniontech.com/task-view-204449.html
    Influence: 个性化-设置缩放-通知
    Change-Id: I8a82d70c9464f3fe12c8f935da7fd7088188e96d

---
+++ misc/translations/translations/dde-appearance_zh_CN.ts
@@ -0,0 +1,29 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="zh_CN">
+<context>
+    <name>AppearanceManager</name>
+    <message>
+        <source>Setting display scaling</source>
+        <translation>即将设置屏幕缩放</translation>
+    </message>
+    <message>
+        <source>Display scaling</source>
+        <translation>屏幕缩放</translation>
+    </message>
+    <message>
+        <source>Log Out Now</source>
+        <translation>立即注销</translation>
+    </message>
+    <message>
+        <source>Later</source>
+        <translation>稍后注销</translation>
+    </message>
+    <message>
+        <source>Log out for display scaling settings to take effect</source>
+        <translation>屏幕缩放需注销后才能生效</translation>
+    </message>
+    <message>
+        <source>Set successfully</source>
+        <translation>设置成功</translation>
+    </message>
+</context>
+</TS>
