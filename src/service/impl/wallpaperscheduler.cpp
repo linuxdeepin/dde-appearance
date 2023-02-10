6fbe0cf790ed399d7e67aaf2aca4d68d38eb7220
dengbo <dengbo@uniontech.com>
2023-02-10 10:12:58 +0800

    chore: 完善代码开源合规
    新增代码文件开源协议
    
    Log: 代码开源协议完善
    Task: https://pms.uniontech.com/task-view-241395.html
    Influence: 无
    Change-Id: I7f14a48597949b2aa1f8fdb4160c29695b67562c

--- src/service/impl/wallpaperscheduler.cpp
+++ src/service/impl/wallpaperscheduler.cpp
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2021 - 2023 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 #include "wallpaperscheduler.h"
 #include "../modules/api/utils.h"
 #include "../modules/common/commondefine.h"
--- src/service/modules/background/backgrounds.h
+++ src/service/modules/background/backgrounds.h
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2021 - 2023 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 #ifndef BACKGROUNDPRIVATE_H
 #define BACKGROUNDPRIVATE_H
 #include <QDir>
