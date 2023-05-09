daad4a4c0a3471830cec85c33fa803d99740fcda
Zhang Dingyuan <zhangdingyuan@deepin.org>
2023-05-09 10:29:49 +0800

    fix: toggle window effect not working
    use org.kde.kwin.Compositing.suspend function.

--- src/service/dbus/deepinwmfaker.cpp
+++ src/service/dbus/deepinwmfaker.cpp
@@ -983,10 +983,14 @@ void DeepinWMFaker::setCompositingEnabled(bool on)
         return;
     }
 
-    if (on)
-        Q_EMIT ResumeCompositorChanged(1);
-    else
-        Q_EMIT SuspendCompositorChanged(1);
+    QDBusInterface composite("org.kde.KWin", "/Compositor", "org.kde.kwin.Compositing");
+
+    if (on) {
+        composite.call("resume");
+    }
+    else {
+        composite.call("suspend");
+    }
 
     // !on 时说明再关闭窗口特效，关闭特效往往都能成功，因此不再需要判断是否成功（KWin中给出值时有些延迟，导致未能及时获取到值）
     if (!on || compositingEnabled() == on)
