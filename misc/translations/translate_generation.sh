8cf12314902ba92ada995d4f0b7e3c21b8420860
Hillwood Yang <hillwood@opensuse.org>
2025-06-09 22:46:48 +0800

    feat: Improve lrelease command lookup logic
    Previously, the script directly invoked lrelease, which could lead to errors
    if the command wasn't found in the system's PATH. Now, the script checks the
    following paths in order:
    
        /usr/bin/lrelease6
        /usr/lib/qt6/bin/lrelease
        /usr/lib64/qt6/bin/lrelease
    
    If an lrelease executable is found, its full path is used; otherwise, the script
    will report an error and exit. This makes the script more robust and ensures
    correct generation of translation files across different environments.
    
    Log: This commit enhances the lrelease command lookup logic within the
    translate_generation.sh script.

--- misc/translations/translate_generation.sh
+++ misc/translations/translate_generation.sh
@@ -7,12 +7,23 @@ cd ${PROJECT_DIR}
 echo 当前目录：${CURRENT_DIR}
 echo 工程目录：${PROJECT_DIR}
 
+if [ -f "/usr/bin/lrelease6" ]; then
+    LRELEASE=lrelease6
+elif [ -f "/usr/lib/qt6/bin/lrelease" ]; then
+    LRELEASE=/usr/lib/qt6/bin/lrelease
+elif [ -f "/usr/lib64/qt6/bin/lrelease" ]; then
+    LRELEASE=/usr/lib64/qt6/bin/lrelease
+else
+    echo "lrelease: command not found."
+    exit 1
+fi
+
 ts_list=(`ls translations/*.ts`)
 
 for ts in "${ts_list[@]}"
 do
     printf "\nprocess ${ts}\n"
-    lrelease "${ts}"
+    ${LRELEASE} "${ts}"
 done
 
 cd ${CURRENT_DIR}
