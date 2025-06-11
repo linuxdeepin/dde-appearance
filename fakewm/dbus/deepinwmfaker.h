e8c8b7d49c22b29a5965205858aaca40c11d68db
Hillwood Yang <hillwood@opensuse.org>
2025-06-11 19:53:50 +0800

    fix: correct SLOT syntax to avoid QObject::connect warning
    Fix a Qt runtime warning:
      "qt.core.qobject.connect: Parentheses expected, slot DeepinWMFaker::DeepinWMFaker::onGsettingsDDEAppearanceChanged"

--- fakewm/dbus/deepinwmfaker.h
+++ fakewm/dbus/deepinwmfaker.h
@@ -189,6 +189,8 @@ private:
     QString getWorkspaceBackgroundForMonitor(const int index,const QString &strMonitorName) const;                    // index 为工作区索引，strMonitorName为显示器的名称
     void setWorkspaceBackgroundForMonitor(const int index, const QString &strMonitorName, const QString &uri) const;         // index 为工作区索引，strMonitorName为显示器的名称，uri为图片资源
     bool isX11Platform();
+
+private slots:
 #ifndef DISABLE_DEEPIN_WM
     void onGsettingsDDEAppearanceChanged(const QString &key);
 #endif
